62d3f9621c510d1bf2489601e1ef4442a23105d8
sorenrehkopf <sorenrehkopfsmith@gmail.com>
2018-05-30 22:00:43 -0700

    share target experimenting in progress

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,20 @@
-console.log('in the worker!', navigator);
+self.addEventListener('install', event => {
+	console.log('the install event', event, self);
+});
+
+self.addEventListener('fetch', event => {
+	console.log(event);
+	event.respondWith(
+    caches.match(event.request).then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+    );
+});
+
 if ('actions' in navigator) {
 	console.log('adding event listener!');
 	navigator.actions.addEventListener('share', event => {
